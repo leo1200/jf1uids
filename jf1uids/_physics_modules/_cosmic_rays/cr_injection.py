28411f7668983beb3f0b9a5cadd95dd4d5e0b5b3
Leonard Storcks <leonards360@googlemail.com>
2025-03-13 16:49:46 +0100

    cleanup stuff

--- jf1uids/_physics_modules/_cosmic_rays/cr_injection.py
+++ jf1uids/_physics_modules/_cosmic_rays/cr_injection.py
@@ -1,53 +1,90 @@
 # general
+from typing import Union
 import jax
 import jax.numpy as jnp
 from functools import partial
+
+# typing
+from jaxtyping import Array, Float, jaxtyped
+from beartype import beartype as typechecker
+from typing import Union
+from jf1uids.option_classes.simulation_config import STATE_TYPE
+
+# jf1uids classes
+from jf1uids._physics_modules._cosmic_rays.cosmic_ray_options import CosmicRayParams
+from jf1uids.data_classes.simulation_helper_data import HelperData
+from jf1uids.fluid_equations.registered_variables import RegisteredVariables
+from jf1uids.option_classes.simulation_config import SimulationConfig
 
 # jf1uids constants
 from jf1uids.option_classes.simulation_config import SPHERICAL
 
 # jf1uids functions
-from jf1uids._physics_modules._cosmic_rays.cr_fluid_equations import total_energy_from_primitives_with_crs
-from jf1uids.shock_finder.shock_finder import find_shock_zone, shock_sensor
+from jf1uids.shock_finder.shock_finder import find_shock_zone
 
-
-
-
+# NOTE: currently only supports 1d setups, TODO: generalize
 
 @partial(jax.jit, static_argnames=['registered_variables', 'config'])
-def inject_crs_at_strongest_shock_radial1D(primitive_state, gamma, helper_data, cosmic_ray_params, config, registered_variables, dt):
+def inject_crs_at_strongest_shock(
+    primitive_state: STATE_TYPE, 
+    gamma: Union[float, Float[Array, ""]],
+    helper_data: HelperData,
+    cosmic_ray_params: CosmicRayParams,
+    config: SimulationConfig,
+    registered_variables: RegisteredVariables,
+    dt: Union[float, Float[Array, ""]]
+) -> STATE_TYPE:
     """
-    Compute the dissipated energy flux due to the strongest shock in the domain.
+    Cosmic ray injection at shock fronts. 
+    Currently only at the strongest shock in the domain.
 
-    https://arxiv.org/abs/1604.07399, section 3.1.2
+    The implementation generally follows
+
+    Pfrommer, Christoph, et al. "Simulating cosmic ray physics on a moving mesh." 
+    Monthly Notices of the Royal Astronomical Society 465.4 (2017): 4500-4529.
+    https://arxiv.org/abs/1604.07399
+
+    and
+
+    Dubois, Yohan, et al. "Shock-accelerated cosmic rays and streaming instability
+    in the adaptive mesh refinement code Ramses."
+    Astronomy & Astrophysics 631 (2019): A121.
+    https://arxiv.org/abs/1907.04300
+
+    Args:
+        primitive_state: The primitive state array.
+        gamma: The adiabatic index.
+        helper_data: The helper data.
+        cosmic_ray_params: The cosmic ray parameters.
+        config: The simulation configuration.
+        registered_variables: The registered variables.
+        dt: The time step.
+
+    Returns:
+        The primitive state array with injected cosmic rays.
 
     """
 
-    # see also
-    # https://github.com/LudwigBoess/DiffusiveShockAccelerationModels.jl/tree/main/src/mach_models
-    # for models regarding the efficiency of the injection
-
+    # the injection efficiency is specified by the user
     injection_efficiency = cosmic_ray_params.diffusive_shock_acceleration_efficiency
+    # in future use e.g. models as implemented in
+    # https://github.com/LudwigBoess/DiffusiveShockAccelerationModels.jl/tree/main/src/mach_models
 
+    # currently for crs the adiabatic indices are hard coded
     gamma_cr = 4/3
     gamma_gas = gamma
 
-    num_cells = primitive_state.shape[1]
-    indices = jnp.arange(num_cells)
+    # find the strongest shock
+    max_shock_idx, left_idx, right_idx = find_shock_zone(
+        primitive_state[registered_variables.pressure_index],
+        primitive_state[registered_variables.velocity_index]
+    )
 
-    sensors = shock_sensor(primitive_state[registered_variables.pressure_index])
-
-    velocity = primitive_state[registered_variables.velocity_index]
-
-    div_v = jnp.zeros_like(velocity)
-    div_v = div_v.at[1:-1].set((velocity[2:] - velocity[:-2]) / 2)
-
-    sensors = jnp.where(div_v < 0, sensors, 0)
-
-    max_shock_idx = jnp.argmax(sensors)
-
-    left_idx, right_idx = find_shock_zone(primitive_state[registered_variables.pressure_index], primitive_state[registered_variables.velocity_index])
-
+    # +2 leads to a smoother transition of the different pressure
+    # components in the shock, but will lead to problems at lower
+    # resolutions, also note the problem that CR pressure injected
+    # in the broadened shock layer experiences PCR∇.u forces
+    # (Dubois et al, 2019), so one might overinject effectively
     left_idx = left_idx + 2
 
     # we only consider a shock moving from left to right
@@ -56,75 +93,45 @@ def inject_crs_at_strongest_shock_radial1D(primitive_state, gamma, helper_data, 
     pre_shock_idx = right_idx + 1
     post_shock_idx = left_idx - 1
 
-    shock_zone_mask = (indices >= left_idx) & (indices <= right_idx)
+    # get a mask for the shock, if an injection as done in Pfommer et al. 2017 is desired
+    # for us it proved to be simpler and similar in results to just inject in one post-shock cell
+    # indices = jnp.arange(num_cells)
+    # shock_zone_mask = (indices >= left_idx) & (indices <= right_idx)
+    # shock_zone_size = jnp.sum(shock_zone_mask)
 
-    shock_zone_size = jnp.sum(shock_zone_mask)
+    # get the pre shock (upstream) state
+    rho1 = primitive_state[registered_variables.density_index, pre_shock_idx] # density
+    P1 = primitive_state[registered_variables.pressure_index, pre_shock_idx] # total pressure
+    P1_CRs = primitive_state[registered_variables.cosmic_ray_n_index, pre_shock_idx] ** gamma_cr # cosmic ray pressure
+    P1_gas = P1 - P1_CRs # gas pressure
+    e1_gas = P1_gas / (gamma_gas - 1) # gas energy / volume
+    e1_crs = P1_CRs / (gamma_cr - 1) # cosmic ray energy / volume
 
-    # jax.debug.print("shock_zone_size: {sz}", sz = shock_zone_size)
-
-
-    # get the pre and post shock states
-    rho1 = primitive_state[registered_variables.density_index, pre_shock_idx]
-    P1 = primitive_state[registered_variables.pressure_index, pre_shock_idx]
-    P1_CRs = primitive_state[registered_variables.cosmic_ray_n_index, pre_shock_idx] ** gamma_cr
-    P1_gas = P1 - P1_CRs
-    e1_gas = P1_gas / (gamma_gas - 1)
-    e1_crs = P1_CRs / (gamma_cr - 1)
-    e1 = e1_gas + e1_crs
-
+    # get the post shock state
     rho2 = primitive_state[registered_variables.density_index, post_shock_idx]
     P2 = primitive_state[registered_variables.pressure_index, post_shock_idx]
     P2_CRs = primitive_state[registered_variables.cosmic_ray_n_index, post_shock_idx] ** gamma_cr
     P2_gas = P2 - P2_CRs
     e2_gas = P2_gas / (gamma_gas - 1)
     e2_crs = P2_CRs / (gamma_cr - 1)
-    e2 = e2_gas + e2_crs
 
+    # get the effective adiabatic index
     gamma_eff1 = (gamma_cr * P1_CRs + gamma_gas * P1_gas) / P1
-    gamma_eff2 = (gamma_cr * P2_CRs + gamma_gas * P2_gas) / P2
 
-    # jax.debug.print("gamma_eff1: {gamma_eff1}, gamma_eff2: {gamma_eff2}", gamma_eff1 = gamma_eff1, gamma_eff2 = gamma_eff2)
-
+    # calculate the pre-shock sound speed
     c1 = jnp.sqrt(gamma_eff1 * P1 / rho1)
 
-    e_th1 = P1_gas / ((gamma_gas - 1))
-    e_th2 = P2_gas / ((gamma_gas - 1))
-
-    e_cr1 = P1_CRs / (gamma_cr - 1)
-    e_cr2 = P2_CRs / (gamma_cr - 1)
-
+    # density ratio
     x_s = rho2 / rho1
 
-    e_diss = e_th2 - e_th1 * x_s ** gamma_gas + e_cr2 - e_cr1 * x_s ** gamma_cr
-
-    # jax.debug.print("blbl")
-
-    # M_1_sq = (P2 / P1 - 1) * x_s / (gamma_eff * (x_s - 1))
-
-    gamma_t = P2 / P1
-
-    # jax.debug.print("gamma_t: {gamma_t}", gamma_t = gamma_t)
-
-    gamma1 = P1/e1 + 1
-    gamma2 = P2/e2 + 1
-    C = ((gamma2 + 1) * gamma_t + gamma2 - 1) * (gamma1 - 1)
-
-                        # or 2 (?) like in Shock-accelerated cosmic rays and streaming instability in the adaptive mesh refinement code Ramses
-    # M_1_sq = 1/gamma_eff1 * (gamma_t - 1) * C / (C - (gamma1 + 1 + (gamma1 - 1) * gamma_t) * (gamma2 - 1))
-
+    # pre-shock mach number
     M_1_sq = (P2 / P1 - 1) * x_s / (gamma_eff1 * (x_s - 1))
+    # alternatively one might use formula 16 in Dubois et al, 2019
 
-    # jax.debug.print("M1: {M1}", M1 = jnp.sqrt(M_1_sq))
-    
+    # dissipated energy density
+    e_diss = e2_gas - e1_gas * x_s ** gamma_gas + e2_crs - e1_crs * x_s ** gamma_cr
 
-
-    # print rho2 and rho1
-    # jax.debug.print("rho2: {rho2}, rho1: {rho1}", rho2 = rho2, rho1 = rho1)
-    
-    # only inject if the left index is positive, if left and right index are less than 20 apart and P2 > P1
-    # criterion = (left_idx > 0) & (right_idx - left_idx < 20) & (P2 > P1) & (M_1_sq > M1_crit ** 2)
-    # injection_efficiency = jax.lax.cond(criterion, lambda _: 0.0, lambda _: injection_efficiency, None)
-
+    # dissipated flux
     f_diss = e_diss * jnp.sqrt(M_1_sq) * c1 / x_s
 
     # calculate the shock surface
@@ -134,38 +141,17 @@ def inject_crs_at_strongest_shock_radial1D(primitive_state, gamma, helper_data, 
     else:
         shock_surface = config.grid_spacing ** (config.dimensionality - 1)
 
-    # jax.debug.print("shock_zone_size {sz}", sz = shock_zone_size)
-
+    # energy to be injected in the form of cosmic ray pressure
     DeltaE_CR = f_diss * shock_surface * dt * injection_efficiency
 
-    # TODO: finish, think about coupling to the energy equation,
-    # so also remove from kinetic energy, adapt velocity accordingly
-
-    # leads to crash in the beginning 
-    # total energy is still an energy density, so we need to multiply by the volume to get the total energy
-    # E_tot = total_energy_from_primitives_with_crs(primitive_state, registered_variables) * helper_data.cell_volumes
-
-    # P = primitive_state[registered_variables.pressure_index]
-    # PCR = primitive_state[registered_variables.cosmic_ray_n_index] ** gamma_cr
-    # PGAS = P - PCR
-    # E_tot = (PGAS / (gamma_gas - 1) + PCR / (gamma_cr - 1)) * helper_data.cell_volumes
-
-    # deltaEtot = jnp.sum(jnp.where(shock_zone_mask, E_tot - E_tot[pre_shock_idx], 0))
-
-    # deltaE_CR_shock_zone = jnp.where(shock_zone_mask, DeltaE_CR * (E_tot - E_tot[pre_shock_idx]) / deltaEtot, 0) / helper_data.cell_volumes
-
-    # or just this
-    # deltaE_CR_shock_zone = jnp.where(shock_zone_mask, DeltaE_CR, 0) / helper_data.cell_volumes / shock_zone_size
-
-    # p_cr = primitive_state[registered_variables.cosmic_ray_n_index] ** gamma_cr
-    # p_cr_new = p_cr + deltaE_CR_shock_zone * (gamma_cr - 1)
-    # n_cr_new = p_cr_new ** (1/gamma_cr)
-
-    # primitive_state = primitive_state.at[registered_variables.cosmic_ray_n_index].set(n_cr_new)
-
+    # inject into a single post-shock cell
     injection_index = left_idx + 1
+
+    # to be injected cosmic ray pressure
     p_cr_injection = primitive_state[registered_variables.cosmic_ray_n_index, injection_index] ** gamma_cr
+    # updated cosmic ray pressure
     p_cr_injection_new = p_cr_injection + DeltaE_CR / helper_data.cell_volumes[injection_index] * (gamma_cr - 1)
+    # note that we work with n_cr = P_CR ^ (1 / gamma_cr) to describe the cosmic rays
     n_cr_injection_new = p_cr_injection_new ** (1/gamma_cr)
     primitive_state = primitive_state.at[registered_variables.cosmic_ray_n_index, injection_index].set(n_cr_injection_new)
 
@@ -173,22 +159,5 @@ def inject_crs_at_strongest_shock_radial1D(primitive_state, gamma, helper_data, 
     # takes away gas pressure, if we want to take the energy from the kinetic energy, 
     # we would have to increase the total pressure and adapt the velocity to 
     # conserve energy
-
-    # # also add to the total pressure
-    # delta_p_shock = deltaE_CR_shock_zone * (gamma_cr - 1)
-    # primitive_state = primitive_state.at[registered_variables.pressure_index].add(delta_p_shock)
-
-    # # and update the velocity to conserve energy
-    # v = primitive_state[registered_variables.velocity_index]
-    # rho = primitive_state[registered_variables.density_index]
-    # v_new = jnp.sqrt((0.5 * rho * v ** 2 - deltaE_CR_shock_zone) / (0.5 * rho)) # problem of negative values
-    # primitive_state = primitive_state.at[registered_variables.velocity_index].set(v_new)
-
-    # TODO: shock sharpening
-    # ratio = P2_CRs / P2
-
-    # mini_mask = (indices >= right_idx - 2) & (indices <= right_idx)
-    # primitive_state = primitive_state.at[registered_variables.cosmic_ray_n_index].set(jnp.where(mini_mask, (primitive_state[registered_variables.pressure_index] * ratio) ** (1/gamma_cr), primitive_state[registered_variables.cosmic_ray_n_index]))
-
 
     return primitive_state
--- jf1uids/shock_finder/shock_finder.py
+++ jf1uids/shock_finder/shock_finder.py
@@ -1,75 +1,87 @@
+# general
 from functools import partial
 import jax.numpy as jnp
 import jax
 
-# @partial(jax.jit, static_argnames=['registered_variables'])
-# def shock_criteria1D(primitive_state, registered_variables):
-#     """
-#     Compute the shock criteria for each cell in the domain.
-#     """
+# typing
+from typing import Tuple, Union
+from jf1uids.option_classes.simulation_config import FIELD_TYPE
+from jaxtyping import Array, Int, jaxtyped
+from beartype import beartype as typechecker
+from typing import Union
 
-#     # Extract the pressure
-#     velocity = primitive_state[registered_variables.velocity_index]
-#     pressure = primitive_state[registered_variables.pressure_index]
-#     density = primitive_state[registered_variables.density_index]
-
-#     # central difference 1d divergence
-#     div_v = jnp.zeros_like(velocity)
-#     div_v = div_v.at[1:-1].set((velocity[2:] - velocity[:-2]) / 2)
-
-#     return div_v < 0
+# NOTE: currently only works for 1d setups, TODO: generalize
 
 @jax.jit
-def shock_sensor(pressure):
+def shock_sensor(pressure: FIELD_TYPE) -> FIELD_TYPE:
     """
-    WENO-JS smoothness indicator for shock detection.
+    WENO-JS 1D smoothness indicator for shock detection.
+
+    Args:
+        pressure: the 1d pressure
+
+    Returns:
+        shock sensors, high where large pressure jumps
+
     """
+
     shock_sensors = jnp.zeros_like(pressure)
-    shock_sensors = shock_sensors.at[1:-1].set((1/4 * (pressure[2:] - pressure[:-2])**2 + 13 / 12 * (pressure[2:] - 2 * pressure[1:-1] + pressure[:-2])))
+    shock_sensors = shock_sensors.at[1:-1].set(
+        1/4 * (pressure[2:] - pressure[:-2])**2 +
+        13 / 12 * (pressure[2:] - 2 * pressure[1:-1] + pressure[:-2])
+    )
+
     return shock_sensors
 
 @jax.jit
-def find_shock_zone(pressure, velocity):
+def find_shock_zone(
+    pressure: FIELD_TYPE,
+    velocity: FIELD_TYPE
+) -> Tuple[Union[int, Int[Array, ""]], Union[int, Int[Array, ""]], Union[int, Int[Array, ""]]]:
+    """
+    Find a numerically broadened shock region based of the strongest shock based
+    on the result of the shock_sensor function and the pressure difference
+    between adjacent cells. Assumes a shock front moving left to right.
+
+    Args:
+        pressure: 1d pressure
+        velocity: 1d velocity
+
+    Returns:
+        index of max shock sensor,
+        left boundary of broadened shock,
+        right boundary of broadened shock
+    
+    """
 
     num_cells = pressure.shape[0]
 
     sensors = shock_sensor(pressure)
 
+    # rule out regions with div v >= 0, see criterion 1 in Pfrommer et al, 2017
     div_v = jnp.zeros_like(velocity)
     div_v = div_v.at[1:-1].set((velocity[2:] - velocity[:-2]) / 2)
-
     sensors = jnp.where(div_v < 0, sensors, 0)
 
     max_shock_idx = jnp.argmax(sensors)
 
+    # bound on the shock sensor
     bound_val = 0.05 * jnp.max(sensors)
 
+    # calculate differences in pressure
     to_next_pressure_differences = jnp.zeros_like(pressure)
     to_next_pressure_differences = jnp.abs(to_next_pressure_differences.at[:-1].set(pressure[1:] - pressure[:-1]))
 
+    # bound on the change in pressure between adjacent cells compared
+    # to the pressure jump at the max_shock_index
     bound_diff = 0.1 * to_next_pressure_differences[max_shock_idx]
 
-    # find the first index left and right
-    # of the max_shock_idx where the sensor is
-    # smaller than bound
-
+    # left index: closest left index where pressure_difference < bound_diff
+    # right index: closest right index where sensor < bound_val or pressure_difference < bound_diff
     indices = jnp.arange(num_cells)
     left_indices = jnp.where((indices < max_shock_idx) & ((to_next_pressure_differences < bound_diff)), indices, -1)
     right_indices = jnp.where((indices > max_shock_idx) & ((sensors < bound_val) | (to_next_pressure_differences < bound_diff)), indices, num_cells)
-
     left_idx = jnp.max(left_indices)
     right_idx = jnp.min(right_indices)
 
-    return left_idx, right_idx
-
-
-# @partial(jax.jit, static_argnames=['registered_variables'])
-# def strongest_shock_radius(primitive_state, helper_data, registered_variables):
-#     """
-#     Compute the radius of the strongest shock in the domain.
-#     """
-#     r = helper_data.geometric_centers
-#     shock_sensors = shock_sensor(primitive_state[registered_variables.pressure_index])
-#     max_shock_idx = jnp.argmax(shock_sensors)
-#     return (r[max_shock_idx - 1] * shock_sensors[max_shock_idx - 1] + r[max_shock_idx] * shock_sensors[max_shock_idx] + r[max_shock_idx + 1] * shock_sensors[max_shock_idx + 1]) / (shock_sensors[max_shock_idx - 1] + shock_sensors[max_shock_idx] + shock_sensors[max_shock_idx + 1])
-
+    return max_shock_idx, left_idx, right_idx
