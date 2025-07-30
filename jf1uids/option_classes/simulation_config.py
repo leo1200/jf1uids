12f6138b0280d29aa7ecd544b0b4daaa03d941de
Leonard Storcks <leonards360@googlemail.com>
2025-07-30 21:07:39 +0200

    minor config stuff

--- jf1uids/option_classes/simulation_config.py
+++ jf1uids/option_classes/simulation_config.py
@@ -261,12 +261,16 @@ def finalize_config(config: SimulationConfig, state_shape) -> SimulationConfig:
             print("Setting minmod limiter for spherical geometry")
             config = config._replace(limiter = MINMOD)
 
+        if config.time_integrator != MUSCL:
+            print("Setting MUSCL time integrator for spherical geometry")
+            config = config._replace(time_integrator = MUSCL)
+
     if (config.limiter == MINMOD or config.limiter == DOUBLE_MINMOD or config.limiter == SUPERBEE or config.riemann_solver == HLL or config.riemann_solver == HLLC or config.riemann_solver == HLLC_LM) and config.split == UNSPLIT:
         print("Due to different implementation approaches, currently only a limited set of Riemann solver and limiters work in unsplit mode, namely LAX_FRIEDRICHS with VAN_ALBADA limiter.")
         print("Switching to split mode.")
         config = config._replace(split = SPLIT)
+        config = config._replace(time_integrator = MUSCL)
         
-
     # set boundary conditions if not set
     if config.boundary_settings is None:
 
