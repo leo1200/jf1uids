740f1615f47d12b15315f550d3175c6a114b493c
n-knoell <nikkn1003@gmail.com>
2025-08-18 11:52:42 +0200

    added working binary_paper.py

--- tests/binary/binary_paper.py
+++ tests/binary/binary_paper.py
@@ -35,7 +35,7 @@ from jf1uids.option_classes.simulation_config import (
     PERIODIC_BOUNDARY, OPEN_BOUNDARY, REFLECTIVE_BOUNDARY, 
     BoundarySettings, BoundarySettings1D
 )
-jax.config.update('jax_enable_x64', True)
+# jax.config.update('jax_enable_x64', True)
 
 print("Setting up simulation...")
 
@@ -43,8 +43,8 @@ print("Setting up simulation...")
 gamma = 5/3
 
 # spatial domain
-box_size = 4.0
-num_cells = 128  #128
+box_size = 5.6
+num_cells = 50  #128
 
 fixed_timestep = False
 dt_max = 0.001
@@ -59,7 +59,7 @@ config = SimulationConfig(
     progress_bar = True,
     binary_config = BinaryConfig(
         binary = True,
-        deposit_particles = "cic"  # Options: "ngp", "cic", "tsc"
+        deposit_particles = "ngp"  # Options: "ngp", "cic", "tsc"
     ),
     self_gravity = True,
     self_gravity_version = self_gravity_version,
@@ -74,7 +74,7 @@ config = SimulationConfig(
     # time_integrator = MUSCL,
     riemann_solver = HLL,
     return_snapshots = True,
-    num_snapshots = 100,
+    num_snapshots = 50,
     boundary_settings = BoundarySettings(
         BoundarySettings1D(
             left_boundary = OPEN_BOUNDARY,
@@ -110,22 +110,32 @@ z_cyl = geometric_centers[..., 2] - config.box_size / 2
 ################################################################################
 
 helper_data = get_helper_data(config)
+
+cell_width = config.box_size / config.num_cells
+mass_source = 4e-3 # works 
+rho_source = mass_source / (cell_width ** 3)
+print("mass both stars:", 2*mass_source, "density star: ", rho_source)
+masses = jnp.array([mass_source, mass_source])  
+
 x1=0.5
 x2=-0.5
-txv1 = jnp.array([0.0, x1, 0.0, 0.0, 0.0, 0.5, 0.0], dtype=jnp.float64)   
-txv2 = jnp.array([0.0, x2, 0.0, 0.0, 0.0,-0.5, 0.0], dtype=jnp.float64)
-masses = jnp.array([0.5, 0.5], dtype=jnp.float64)  # Masses of the two point masses
+v_phi0 = jnp.sqrt(mass_source/2)
+print("Azimuthal vel of each star: ", v_phi0)
+txv1 = jnp.array([0.0, x1, 0.0, 0.0, 0.0, -v_phi0, 0.0])    #this is clockwise rotation in the xy plane
+txv2 = jnp.array([0.0, x2, 0.0, 0.0, 0.0, v_phi0, 0.0])
 binary_state = jnp.concatenate([txv1, txv2])
 
 binary_params = BinaryParams(
     masses = masses,
     binary_state = binary_state
 )
+
+T_orb = 2*jnp.pi*(x1-x2) / 2 / v_phi0   #T_orb at r=0.5
 params = SimulationParams(
     C_cfl = 0.4,
     dt_max = dt_max,
     gamma = gamma,
-    t_end = 40,  # 0.8,
+    t_end = 20,  # 0.8,
     binary_params = binary_params
 )
 
@@ -148,14 +158,12 @@ dx = config.box_size / (config.num_cells - 1)
 # === Circumbinary disc initial conditions (from §4.7) ===
 
 # Physical parameters
-G = 1.0                   # in code units where G M⊙ = 1
-M1 = 0.5                  # primary mass (M⊙)
-M2 = 0.5                  # secondary mass (M⊙)
-Mtot = M1 + M2
+G = 1.0                
+Mtot = 2 * mass_source
 
-R_cav = 2.5               # cavity radius [au]
-delta_0    = 3e-4      #1e-5        # floor parameter
-sigma_0    = 5e-3      # choose normalization for Σ(r)
+R_cav = 2.5          # cavity radius [au]
+delta_0    = 0.5e-7  #1e-5        # floor parameter
+sigma_0    = 1e-5    # choose normalization for Σ(r)
 rsafe = 1e-6
 
 # Compute surface density 
@@ -172,7 +180,7 @@ mask = True   #(r_cyl <= R_o) & (r_cyl >= R_i) & (z_cyl >= -0.3) & (z_cyl <= 0.3
 #     mask,
 #     jnp.clip(sigma / (H * jnp.sqrt(2 * jnp.pi)) * jnp.exp(-0.5 * (z_cyl / H)**2),a_min=1e-8),
 #     1e-4)
-rho = jnp.clip(sigma / (H * jnp.sqrt(2 * jnp.pi)) * jnp.exp(-0.5 * (z_cyl / H)**2),a_min=1e-6)
+rho = jnp.clip(sigma / (H * jnp.sqrt(2 * jnp.pi)) * jnp.exp(-0.5 * (z_cyl / H)**2),a_min=1e-9)
 # rho = sigma / (H * jnp.sqrt(2 * jnp.pi)) * jnp.exp(-0.5 * (z_cyl / H)**2)
 
 
@@ -185,7 +193,7 @@ rho = jnp.clip(sigma / (H * jnp.sqrt(2 * jnp.pi)) * jnp.exp(-0.5 * (z_cyl / H)**
 cs = (H / (r_cyl + rsafe)) * jnp.sqrt(G * Mtot / (r_cyl + rsafe))
 
 # Gas pressure 
-P = jnp.clip(rho * cs**2,a_min=1e-8)
+P = jnp.clip(rho * cs**2,a_min=1e-9)
 
 # Cartesian gradients of P
 dP_dx = jnp.gradient(P, dx, axis=0)
@@ -198,10 +206,9 @@ uy = y_cyl / (r_cyl + rsafe)
 # project the pressure gradient onto the radial direction:
 dP_dr = dP_dx * ux + dP_dy * uy
 
-rho_safe = 1e-6
 #subkeplerian velocity profile
 v_phi = jnp.sqrt(
-    jnp.clip(G * Mtot / (r_cyl + rsafe) + (r_cyl / (rho + rho_safe)) * dP_dr, a_min=0.0)  # avoid negative values
+    jnp.clip(G * Mtot / (r_cyl + rsafe) + (r_cyl / (rho)) * dP_dr, a_min=0.0)  # avoid negative values
 )
 
 ### kelperian velocity profile
@@ -210,7 +217,7 @@ v_phi = jnp.sqrt(
 # )
 
 # decompose into cartesian velocities
-v_x = -v_phi *  y_cyl / (r_cyl + rsafe)
+v_x = -v_phi *  y_cyl / (r_cyl + rsafe)   ## this is clockwise rotation in the xy plane
 v_y =  v_phi *  x_cyl / (r_cyl + rsafe)
 v_z = jnp.zeros_like(v_x)
 
@@ -219,8 +226,12 @@ v_z = jnp.zeros_like(v_x)
 # v_y=jnp.zeros_like(rho)
 # v_z=jnp.zeros_like(rho)
 
-total_mass = jnp.sum(rho) * dx**3
-print(f"Total disc mass: {total_mass:.3e}")
+disc_mass=jnp.sum(rho) * cell_width**3     ## use the cell_width definition  (WHY dx???)
+# print(f"Total disc mass: {total_mass:.3e}")
+print(f"Total disc mass: {disc_mass:.3e}")
+print("M_disc/M_star: ", disc_mass/Mtot)
+print("vx_max: ", jnp.max(v_x), "vy_max: ", jnp.max(v_y))
+
 # =========================================
 
 # initial thermal energy per unit mass = 0.05
@@ -238,15 +249,16 @@ initial_state = construct_primitive_state(
     gas_pressure = P,
 )
 
+"""
 import numpy as np
 
 def report(name, arr, near_zero_tol=0.0, max_neighborhood=3):
-    """
-    Print diagnostics for `arr` (a JAX or numpy array).
-    - near_zero_tol: if >0, treat abs(x) <= near_zero_tol as "zero" (useful for underflow).
-                     if ==0 (default), checks exact zeros.
-    - max_neighborhood: number of cells per axis to show around the first zero (<=3 recommended).
-    """
+    
+    # Print diagnostics for `arr` (a JAX or numpy array).
+    # - near_zero_tol: if >0, treat abs(x) <= near_zero_tol as "zero" (useful for underflow).
+    #                  if ==0 (default), checks exact zeros.
+    # - max_neighborhood: number of cells per axis to show around the first zero (<=3 recommended).
+    
     a = jax.device_get(arr)           # bring to host
     a = np.asarray(a)                 # ensure numpy
     size = a.size
@@ -318,7 +330,7 @@ report("dP_dy", dP_dy)
 report("dP_dr", dP_dr)
 report("v_phi", v_phi)
 report("initial_state", initial_state)
-
+"""
 
 config = finalize_config(config, initial_state.shape)
 
@@ -330,27 +342,28 @@ b = num_cells // 2 + 30
 c = num_cells // 2 + 20
 d = num_cells // 2 + 50
 
+save_path = os.path.join('Binary_paper')
 plt.imshow(jnp.abs(initial_state[registered_variables.density_index, :, :, num_cells // 2].T), cmap = "jet", origin = "lower", extent=[0, box_size, 0, box_size], norm = LogNorm())
 plt.colorbar()
 plt.xlabel("x")
 plt.ylabel("y")
-plt.savefig("Binary_paper/initial_rho.png")
+plt.savefig(save_path+"/initial_xy.png")
 plt.close()
 
-plt.imshow(jnp.abs(initial_state[registered_variables.density_index,  num_cells // 2, :, :].T), cmap = "jet", origin = "lower", extent=[0, box_size, 0, box_size], norm = LogNorm())
+plt.imshow(jnp.abs(initial_state[registered_variables.density_index, num_cells // 2, :, :].T), cmap = "jet", origin = "lower", extent=[0, box_size, 0, box_size], norm = LogNorm())
 plt.colorbar()
 plt.xlabel("x")
-plt.ylabel("z")
-plt.savefig("Binary_paper/initial_rho_side.png")
+plt.ylabel("y")
+plt.savefig(save_path+"/initial_xz.png")
 plt.close()
+
 
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4))
 
+
 ax1.scatter(r_cyl.flatten(), initial_state[registered_variables.density_index].flatten(), label="Final Density", s = 1)
-# x and y log scale
-# ax1.set_xscale("log")
-# ax1.set_yscale("log")
 ax1.set_xlabel("r")
+ax1.set_yscale("log")
 ax1.set_ylabel("Density")
 
 # velocity profile
@@ -358,23 +371,22 @@ v_r = jnp.sqrt(initial_state[registered_variables.velocity_index.x] ** 2 + initi
 
 ax2.scatter(r_cyl.flatten(), v_r.flatten(), label="Radial Velocity", s = 1)
 # ax2.set_xscale("log")
+ax2.set_ylim(0,0.1)
 ax2.set_xlabel("r")
 ax2.set_ylabel("Velocity")
 
 # plot P / rho^gamma
 
 ax3.scatter(r_cyl.flatten(), initial_state[registered_variables.pressure_index].flatten(), label="P / rho^gamma", s = 1)
+ax3.set_ylim(0,1e-7)
 ax3.set_xlabel("r")
 ax3.set_ylabel("Pressure")
 # ax3.set_xscale("log")
-# ax3.set_xlim(1e-2, 6e-1)
 
 fig.suptitle("Binary")
-
 plt.tight_layout()
-fig.savefig("Binary_paper/initial_variables.png")
+fig.savefig(save_path+"/initial_variables.png")
 plt.close()
-
 
 result = time_integration(initial_state, config, params, helper_data, registered_variables)
 final_state = result.states[-1]
@@ -386,40 +398,63 @@ b = num_cells // 2 + 30
 
 c = num_cells // 2 + 20
 d = num_cells // 2 + 50
-
-plt.imshow(jnp.abs(final_state[registered_variables.pressure_index, :, :, num_cells // 2].T), cmap = "jet", origin = "lower", extent=[0, box_size, 0, box_size], norm = LogNorm())
+idx_rho=registered_variables.density_index
+idx_p=registered_variables.pressure_index
+plt.imshow(jnp.abs(final_state[idx_rho, :, :, num_cells // 2]), cmap = "jet", origin = "lower", extent=[0, box_size, 0, box_size], norm = LogNorm())
 plt.colorbar()
 plt.xlabel("x")
 plt.ylabel("y")
-plt.savefig("Binary_paper/endstate.png")
-# plot the radial density profile rho over r in a log-log plot
+plt.savefig(save_path+"/endstate_rho.png")
 
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4))
 
-ax1.scatter(r_cyl.flatten(), final_state[registered_variables.density_index].flatten(), label="Final Density", s = 1)
-# x and y log scale
+ax1.scatter(r_cyl[:, :, num_cells // 2].flatten(), final_state[registered_variables.density_index, :, :, num_cells // 2].flatten(), label="Final Density", s = 1)
 # ax1.set_xscale("log")
-# ax1.set_yscale("log")
+ax1.set_yscale("log")
 ax1.set_xlabel("r")
 ax1.set_ylabel("Density")
 
 # velocity profile
-v_r = jnp.sqrt(final_state[registered_variables.velocity_index.x] ** 2 + final_state[registered_variables.velocity_index.y] ** 2 + final_state[registered_variables.velocity_index.z] ** 2)
+# v_r = jnp.sqrt(final_state[registered_variables.velocity_index.x] ** 2 + final_state[registered_variables.velocity_index.y] ** 2 + final_state[registered_variables.velocity_index.z] ** 2)
+v_r = jnp.sqrt(final_state[registered_variables.velocity_index.x, :, :, num_cells // 2] ** 2 + final_state[registered_variables.velocity_index.y, :, :, num_cells // 2] ** 2 + final_state[registered_variables.velocity_index.z, :, :, num_cells // 2] ** 2)
 
-ax2.scatter(r_cyl.flatten(), v_r.flatten(), label="Radial Velocity", s = 1)
+ax2.scatter(r_cyl[:, :, num_cells // 2].flatten(), v_r.flatten(), label="Radial Velocity", s = 1)
 ax2.set_xlabel("r")
 ax2.set_ylabel("Velocity")
 
 # plot P / rho^gamma
 
-ax3.scatter(r_cyl.flatten(), final_state[registered_variables.pressure_index].flatten(), label="P / rho^gamma", s = 1)
+ax3.scatter(r_cyl[:, :, num_cells // 2].flatten(), final_state[registered_variables.pressure_index, :, :, num_cells // 2].flatten(), label="P / rho^gamma", s = 1)
 ax3.set_xlabel("r")
 ax3.set_ylabel("Pressure")
 
-fig.suptitle("BINARY")
+fig.suptitle("Binary")
 
 plt.tight_layout()
-fig.savefig("Binary_paper/var_finals.png")
+fig.savefig(save_path+"/var_finals.png")
+
+
+
+# ## Conservational properties
+# config = config._replace(return_snapshots = True, num_snapshots = 60)
+# params = params._replace(t_end = 3.0)
+
+# snapshots = time_integration(initial_state, config, params, helper_data, registered_variables)
+# total_energy = snapshots.total_energy
+# internal_energy = snapshots.internal_energy
+# kinetic_energy = snapshots.kinetic_energy
+# gravitational_energy = snapshots.gravitational_energy
+# total_mass = snapshots.total_mass
+# time = snapshots.time_points
+# t_end = 3.0
+# plt.plot(time, total_energy, label="Total Energy", color = "black")
+# plt.plot(time, internal_energy, label="Internal Energy", color = "green")
+# plt.plot(time, kinetic_energy, label="Kinetic Energy", color = "red")
+# plt.plot(time, gravitational_energy, label="Gravitational Energy", color = "blue")
+# plt.xlabel("Time")
+# plt.ylabel("Energy")
+# plt.legend()
+# plt.savefig("Binary/energy_conservation.png")
 
 import matplotlib.animation as animation
 
@@ -438,15 +473,14 @@ def animate(i):
 ani = animation.FuncAnimation(fig, animate, frames=len(result.states), interval=100)
 plt.colorbar(ax.imshow(result.states[0][0, :, :, num_cells // 2].T, cmap="jet", origin="lower", extent=[0, box_size, 0, box_size], norm=LogNorm()), ax=ax)
 # save to gif
-ani.save("Binary_paper/rho_ani.gif")
+ani.save(save_path+"/ani_rho_xy.gif")
 plt.close(fig)
 
 fig, ax = plt.subplots()
-
 def animate(i):
     ax.clear()
     state = result.states[i]
-    im = ax.imshow(state[0, :, :, num_cells // 2].T, cmap="jet", origin="lower", extent=[0, box_size, 0, box_size], norm=LogNorm())
+    im = ax.imshow(state[0, num_cells // 2, :, :].T, cmap="jet", origin="lower", extent=[0, box_size, 0, box_size], norm=LogNorm())
     ax.set_xlabel("x")
     ax.set_ylabel("z")
     ax.set_title(f"Density at time {time[i]:.2f}")
@@ -454,7 +488,34 @@ def animate(i):
 
 ani = animation.FuncAnimation(fig, animate, frames=len(result.states), interval=100)
 plt.colorbar(ax.imshow(result.states[0][0, num_cells // 2, :, :].T, cmap="jet", origin="lower", extent=[0, box_size, 0, box_size], norm=LogNorm()), ax=ax)
-# save to gif
-ani.save("Binary_paper/rho_ani_side.gif")
+ani.save(save_path+"/ani_rho_xz.gif")
 plt.close(fig)
 
+fig, ax = plt.subplots()
+def animate(i):
+    ax.clear()
+    state = result.states[i]
+    im = ax.scatter(r_cyl[:, :, num_cells // 2].flatten(), state[0, :, :, num_cells // 2].flatten(), s = 1)
+    ax.set_yscale("log")
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_title(f"Density at time {time[i]:.2f}")
+    return [im]
+
+ani = animation.FuncAnimation(fig, animate, frames=len(result.states), interval=100)
+ani.save(save_path+"/ani_rho_flat.gif")
+plt.close(fig)
+
+fig, ax = plt.subplots()
+def animate(i):
+    ax.clear()
+    state = result.states[i]
+    im = ax.scatter(r_cyl[:, :, num_cells // 2].flatten(), jnp.sqrt(state[registered_variables.velocity_index.x, :, :, num_cells // 2] ** 2 + state[registered_variables.velocity_index.y, :, :, num_cells // 2] ** 2 + state[registered_variables.velocity_index.z, :, :, num_cells // 2] ** 2).flatten(), s = 1)
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_title(f"Velocity at time {time[i]:.2f}")
+    return [im]
+
+ani = animation.FuncAnimation(fig, animate, frames=len(result.states), interval=100)
+ani.save(save_path+"/ani_vel.gif")
+plt.close(fig)
