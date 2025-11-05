e39535e2d342bfa54555b6428fbdded23a135049
Leonard Storcks <leonards360@googlemail.com>
2025-11-05 14:21:11 +0100

    cleanup

--- jf1uids/_finite_difference/_magnetic_update/_constrained_transport.py
+++ jf1uids/_finite_difference/_magnetic_update/_constrained_transport.py
@@ -1,6 +1,21 @@
 """
-Corrected Constrained Transport (CT) implementation for MHD.
+Constrained Transport (CT) implementation for MHD.
 Based on HOW-MHD paper (Seo & Ryu 2023).
+
+Algorithm summary
+-----------------
+
+We carry interface magnetic fields
+
+b_x at x-interfaces,
+b_y at y-interfaces,
+b_z at z-interfaces
+
+through the simulation, updating them using the CT
+algorithm such that (ignoring floating point errors) the
+divergence of B remains zero. This is achieved by updating
+the interfaces based on the discrete curl of an electric
+field defined at cell edges.
 """
 
 import jax
@@ -11,97 +26,98 @@ from jaxtyping import Array, Float
 
 from jf1uids.variable_registry.registered_variables import RegisteredVariables
 
+XAXIS = 0
+YAXIS = 1
+ZAXIS = 2
+
 @partial(jax.jit, static_argnames=["registered_variables", "ct_order"])
 def constrained_transport_rhs(
     conserved_state,
-    bx_interface,  
-    by_interface,  
-    bz_interface,  
-    flux_x,
-    flux_y,
-    flux_z,
-    dtdx,
-    dtdy,
-    dtdz,
+    weno_flux_x,
+    weno_flux_y,
+    weno_flux_z,
+    time_step,
+    grid_spacing,
     registered_variables: RegisteredVariables,
     ct_order: int = 6,
-) -> Tuple[Float[Array, "Nx+1 Ny Nz"], Float[Array, "Nx Ny+1 Nz"], Float[Array, "Nx Ny Nz+1"]]:
+):
     """
-    Update magnetic field at interfaces using constrained transport.
-    
-    Returns:
-        Updated interface magnetic fields (bx_new, by_new, bz_new)
+    Here we compute the RHS updates for the interface magnetic fields.
     """
+
+    # Step 0: retrieve variables
+
+    # at index i, the fluxes are at interfaces i+1/2
+    By_flux_x_interface = weno_flux_x[registered_variables.magnetic_index.y]
+    Bz_flux_x_interface = weno_flux_x[registered_variables.magnetic_index.z]
+    Bx_flux_y_interface = weno_flux_y[registered_variables.magnetic_index.x]
+    Bz_flux_y_interface = weno_flux_y[registered_variables.magnetic_index.z]
+    Bx_flux_z_interface = weno_flux_z[registered_variables.magnetic_index.x]
+    By_flux_z_interface = weno_flux_z[registered_variables.magnetic_index.y]
+
+    # cell-centered variables
+    rho = conserved_state[registered_variables.density_index]
+    vx = conserved_state[registered_variables.momentum_index.x] / rho
+    vy = conserved_state[registered_variables.momentum_index.y] / rho
+    vz = conserved_state[registered_variables.momentum_index.z] / rho
+    Bx = conserved_state[registered_variables.magnetic_index.x]
+    By = conserved_state[registered_variables.magnetic_index.y]
+    Bz = conserved_state[registered_variables.magnetic_index.z]
     
-    # Get indices
-    DI = registered_variables.density_index
-    MX = registered_variables.momentum_index.x
-    MY = registered_variables.momentum_index.y
-    MZ = registered_variables.momentum_index.z
-    BX = registered_variables.magnetic_index.x
-    BY = registered_variables.magnetic_index.y
-    BZ = registered_variables.magnetic_index.z
+    # Step 1: Compute modified magnetic field fluxes (Eq. 12 - 17)
     
-    # Extract magnetic fluxes from the full flux vectors
-    # These are already at the correct interfaces
-    f_y = flux_x[BY]  # By flux at x-interfaces
-    f_z = flux_x[BZ]  # Bz flux at x-interfaces
-    g_x = flux_y[BX]  # Bx flux at y-interfaces  
-    g_z = flux_y[BZ]  # Bz flux at y-interfaces
-    h_x = flux_z[BX]  # Bx flux at z-interfaces
-    h_y = flux_z[BY]  # By flux at z-interfaces
-    
-    # Get velocity and B field for modified fluxes
-    rho = conserved_state[DI]
-    vx = conserved_state[MX] / rho
-    vy = conserved_state[MY] / rho
-    vz = conserved_state[MZ] / rho
-    Bx = conserved_state[BX]
-    By = conserved_state[BY]
-    Bz = conserved_state[BZ]
-    
-    # Step 1: Compute modified magnetic field fluxes (Equations 12-17)
-    # The WENO fluxes already include the advection terms, but we need to add
-    # the additional terms for the CT algorithm
-    
-    # At x-interfaces
-    Bx_xi = interp4_to_xface(Bx)
-    vy_xi = interp4_to_xface(vy)
-    vz_xi = interp4_to_xface(vz)
-    
-    f_star_y = f_y + Bx_xi * vy_xi  # Modified By flux
-    f_star_z = f_z + Bx_xi * vz_xi  # Modified Bz flux
-    
-    # At y-interfaces  
-    By_yi = interp4_to_yface(By)
-    vx_yi = interp4_to_yface(vx)
-    vz_yi = interp4_to_yface(vz)
-    
-    g_star_x = g_x + By_yi * vx_yi  # Modified Bx flux
-    g_star_z = g_z + By_yi * vz_yi  # Modified Bz flux
+    # At x-interfaces, after the interpolation at index i, 
+    # we have values at i+1/2
+    Bx_x_interface = interp4(Bx, XAXIS)
+    vy_x_interface = interp4(vy, XAXIS)
+    vz_x_interface = interp4(vz, XAXIS)
+
+    By_flux_x_interface_mod = By_flux_x_interface + Bx_x_interface * vy_x_interface
+    Bz_flux_x_interface_mod = Bz_flux_x_interface + Bx_x_interface * vz_x_interface
+
+    # At y-interfaces
+    By_y_interface = interp4(By, YAXIS)
+    vx_y_interface = interp4(vx, YAXIS)
+    vz_y_interface = interp4(vz, YAXIS)
+
+    Bx_flux_y_interface_mod = Bx_flux_y_interface + By_y_interface * vx_y_interface
+    Bz_flux_y_interface_mod = Bz_flux_y_interface + By_y_interface * vz_y_interface
     
     # At z-interfaces
-    Bz_zi = interp4_to_zface(Bz)
-    vx_zi = interp4_to_zface(vx)
-    vy_zi = interp4_to_zface(vy)
-    
-    h_star_x = h_x + Bz_zi * vx_zi  # Modified Bx flux
-    h_star_y = h_y + Bz_zi * vy_zi  # Modified By flux
+    Bz_z_interface = interp4(Bz, ZAXIS)
+    vx_z_interface = interp4(vx, ZAXIS)
+    vy_z_interface = interp4(vy, ZAXIS)
+
+    Bx_flux_z_interface_mod = Bx_flux_z_interface + Bz_z_interface * vx_z_interface
+    By_flux_z_interface_mod = By_flux_z_interface + Bz_z_interface * vy_z_interface
     
     # Step 2: Compute electric field components at cell edges (Equations 19-21)
-    # Omega_z at (x,y) edges: needs interpolation from interfaces to edges
-    g_star_x_edge = interp4_to_xy_edge_from_y(g_star_x)  
-    f_star_y_edge = interp4_to_xy_edge_from_x(f_star_y)  
+    
+    # interpolate from the y interfaces to the (x,y) edges
+    g_star_x_edge = interp4(Bx_flux_y_interface_mod, XAXIS)
+
+    # interpolate from the x interfaces to the (x,y) edges
+    f_star_y_edge = interp4(By_flux_x_interface_mod, YAXIS)
+
+    # electric field component at (x,y) edges
     Omega_z_edge = g_star_x_edge - f_star_y_edge
     
-    # Omega_x at (y,z) edges
-    h_star_y_edge = interp4_to_yz_edge_from_z(h_star_y)
-    g_star_z_edge = interp4_to_yz_edge_from_y(g_star_z)
+    # interpolate from the z interfaces to the (y,z) edges
+    h_star_y_edge = interp4(By_flux_z_interface_mod, YAXIS)
+
+    # interpolate from the y interfaces to the (y,z) edges
+    g_star_z_edge = interp4(Bz_flux_y_interface_mod, ZAXIS)
+
+    # electric field component at (y,z) edges
     Omega_x_edge = h_star_y_edge - g_star_z_edge
-    
-    # Omega_y at (x,z) edges  
-    f_star_z_edge = interp4_to_xz_edge_from_x(f_star_z)
-    h_star_x_edge = interp4_to_xz_edge_from_z(h_star_x)
+
+    # interpolate from the x interfaces to the (z,x) edges
+    f_star_z_edge = interp4(Bz_flux_x_interface_mod, ZAXIS)
+
+    # interpolate from the z interfaces to the (z,x) edges
+    h_star_x_edge = interp4(Bx_flux_z_interface_mod, XAXIS)
+
+    # electric field component at (z,x) edges
     Omega_y_edge = f_star_z_edge - h_star_x_edge
     
     # Step 3: Apply smoothing to edge fluxes (Equation 23 in paper)
@@ -109,68 +125,36 @@ def constrained_transport_rhs(
     Omega_x_bar = smooth_yz_edge(Omega_x_edge)
     Omega_y_bar = smooth_xz_edge(Omega_y_edge)
     
-    # Step 4: Update interface magnetic fields using high-order FD
+    # Update the interface magnetic fields based on the discrete curl of
+    # the electric field at edges (Eq. 24 - 26)
+
     if ct_order == 6:
         c1, c2, c3 = 75.0/64.0, -25.0/384.0, 3.0/640.0
     else:  # ct_order == 4
         c1, c2, c3 = 9.0/8.0, -1.0/24.0, 0.0
-    
-    # Update bx at x-interfaces (Equation 24)
-    # dbx/dt = -dOz/dy + dOy/dz
-    rhs_bx = - dtdy * fd_deriv_y(Omega_z_bar, c1, c2, c3) \
-                          + dtdz * fd_deriv_z(Omega_y_bar, c1, c2, c3)
-    
-    # Update by at y-interfaces (Equation 25)
-    # dby/dt = -dOx/dz + dOz/dx
-    rhs_by = - dtdz * fd_deriv_z(Omega_x_bar, c1, c2, c3) \
-                          + dtdx * fd_deriv_x(Omega_z_bar, c1, c2, c3)
-    
-    # Update bz at z-interfaces (Equation 26)
-    # dbz/dt = -dOy/dx + dOx/dy
-    rhs_bz = - dtdx * fd_deriv_x(Omega_y_bar, c1, c2, c3) \
-                          + dtdy * fd_deriv_y(Omega_x_bar, c1, c2, c3)
+
+    rhs_bx = - time_step / grid_spacing * finite_difference46(Omega_z_bar, c1, c2, c3, YAXIS) \
+             + time_step / grid_spacing * finite_difference46(Omega_y_bar, c1, c2, c3, ZAXIS)
+
+    rhs_by = - time_step / grid_spacing * finite_difference46(Omega_x_bar, c1, c2, c3, ZAXIS) \
+             + time_step / grid_spacing * finite_difference46(Omega_z_bar, c1, c2, c3, XAXIS)
+
+    rhs_bz = - time_step / grid_spacing * finite_difference46(Omega_y_bar, c1, c2, c3, XAXIS) \
+             + time_step / grid_spacing * finite_difference46(Omega_x_bar, c1, c2, c3, YAXIS)
 
     return rhs_bx, rhs_by, rhs_bz
 
 
 # Fourth-order interpolation to interfaces
-def interp4_to_xface(arr):
-    """Interpolate to x-interfaces using 4th order."""
-    # From cell centers to x-faces (between i and i+1)
-    return (-jnp.roll(arr, 1, axis=0) + 9*arr + 9*jnp.roll(arr, -1, axis=0) - jnp.roll(arr, -2, axis=0)) / 16.0
+@partial(jax.jit, static_argnames=["axis"])
+def interp4(arr, axis):
+    """
+    Interpolate to x-interfaces using 4th order
+        f_{i+1/2} = (-f_{i-1} + 9f_{i} + 9f_{i+1} - f_{i+2}) / 16
+    The i-th array index in the output corresponds to the i+1/2 interface.
+    """
+    return (-jnp.roll(arr, 1, axis=axis) + 9*arr + 9*jnp.roll(arr, -1, axis=axis) - jnp.roll(arr, -2, axis=axis)) / 16.0
 
-def interp4_to_yface(arr):
-    """Interpolate to y-interfaces using 4th order."""
-    return (-jnp.roll(arr, 1, axis=1) + 9*arr + 9*jnp.roll(arr, -1, axis=1) - jnp.roll(arr, -2, axis=1)) / 16.0
-
-def interp4_to_zface(arr):
-    """Interpolate to z-interfaces using 4th order."""
-    return (-jnp.roll(arr, 1, axis=2) + 9*arr + 9*jnp.roll(arr, -1, axis=2) - jnp.roll(arr, -2, axis=2)) / 16.0
-
-# Interpolation to edges
-def interp4_to_xy_edge_from_x(arr):
-    """From x-interfaces to (x,y) edges."""
-    return interp4_to_yface(arr)
-
-def interp4_to_xy_edge_from_y(arr):
-    """From y-interfaces to (x,y) edges."""
-    return interp4_to_xface(arr)
-
-def interp4_to_yz_edge_from_y(arr):
-    """From y-interfaces to (y,z) edges."""
-    return interp4_to_zface(arr)
-
-def interp4_to_yz_edge_from_z(arr):
-    """From z-interfaces to (y,z) edges."""
-    return interp4_to_yface(arr)
-
-def interp4_to_xz_edge_from_x(arr):
-    """From x-interfaces to (x,z) edges."""
-    return interp4_to_zface(arr)
-
-def interp4_to_xz_edge_from_z(arr):
-    """From z-interfaces to (x,z) edges."""
-    return interp4_to_xface(arr)
 
 # Smoothing at edges
 def smooth_xy_edge(omega):
@@ -191,26 +175,13 @@ def smooth_xz_edge(omega):
     smooth_z = (jnp.roll(omega, 1, axis=2) - 2*omega + jnp.roll(omega, -1, axis=2)) / 24.0
     return omega + smooth_x + smooth_z
 
-# TODO: do these in double precision?
-
 # Finite difference derivatives
-def fd_deriv_x(omega, c1, c2, c3):
+@partial(jax.jit, static_argnames=["axis"])
+def finite_difference46(omega, c1, c2, c3, axis):
     """High-order FD derivative in x."""
-    return c1 * (omega - jnp.roll(omega, 1, axis=0)) + \
-           c2 * (jnp.roll(omega, -1, axis=0) - jnp.roll(omega, 2, axis=0)) + \
-           c3 * (jnp.roll(omega, -2, axis=0) - jnp.roll(omega, 3, axis=0))
-
-def fd_deriv_y(omega, c1, c2, c3):
-    """High-order FD derivative in y."""
-    return c1 * (omega - jnp.roll(omega, 1, axis=1)) + \
-           c2 * (jnp.roll(omega, -1, axis=1) - jnp.roll(omega, 2, axis=1)) + \
-           c3 * (jnp.roll(omega, -2, axis=1) - jnp.roll(omega, 3, axis=1))
-
-def fd_deriv_z(omega, c1, c2, c3):
-    """High-order FD derivative in z."""
-    return c1 * (omega - jnp.roll(omega, 1, axis=2)) + \
-           c2 * (jnp.roll(omega, -1, axis=2) - jnp.roll(omega, 2, axis=2)) + \
-           c3 * (jnp.roll(omega, -2, axis=2) - jnp.roll(omega, 3, axis=2))
+    return c1 * (omega - jnp.roll(omega, 1, axis=axis)) + \
+           c2 * (jnp.roll(omega, -1, axis=axis) - jnp.roll(omega, 2, axis=axis)) + \
+           c3 * (jnp.roll(omega, -2, axis=axis) - jnp.roll(omega, 3, axis=axis))
 
 # Interpolation from interfaces back to centers
 @partial(jax.jit, static_argnames=["registered_variables"])
@@ -270,8 +241,8 @@ def initialize_interface_fields(
     Bz = conserved_state[BZ]
     
     # Use fourth-order interpolation
-    bx_interface = interp4_to_xface(Bx)
-    by_interface = interp4_to_yface(By)
-    bz_interface = interp4_to_zface(Bz)
-    
+    bx_interface = interp4(Bx, XAXIS)
+    by_interface = interp4(By, YAXIS)
+    bz_interface = interp4(Bz, ZAXIS)
+
     return bx_interface, by_interface, bz_interface
